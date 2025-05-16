fa409e0cca308c29bab1e7e68cd207278b596cf9
mimihimesama <gwa445@naver.com>
2025-05-17 01:46:08 +0900

    🔨 Chore : Redis 환경 변경
    - aioredis 적용
    - port 6380으로 변경
    
    resolves: #17

--- src/init/redis_init.py
+++ src/init/redis_init.py
@@ -1,24 +1,22 @@
-import redis
 from ..config.config import config
+import redis.asyncio as aioredis
 
 # Redis 연결
-redis_client = redis.Redis(
+redis_client = aioredis.Redis(
     host=config['redis']['redisHost'],
     port=config['redis']['redisPort'],
     db=0,
     decode_responses=True,
+    protocol=3,
 )
 
-# Redis 연결 이벤트 핸들러
-def on_connect():
-    print('Redis connected!')
-
-def on_error(err):
-    print('Redis Client Error', err)
-
-# Redis 연결 이벤트 핸들러 등록
-redis_client.ping()  # 연결 테스트
-print('Redis connected!')  # 연결 성공 메시지 출력
+# 연결 테스트용 함수만 정의 (실행은 X)
+async def test_redis_connection():
+    try:
+        await redis_client.ping()
+        print('Redis connected!')
+    except Exception as e:
+        print('Redis connection failed:', e)
 
 # Redis 클라이언트 반환
 redis_client = redis_client 
